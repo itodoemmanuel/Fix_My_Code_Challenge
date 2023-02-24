31b17572b49d6f90f779b8cc601b82770f4aa41c
Itodo Emmanuel Itodo <54606115+itodoemmanuel@users.noreply.github.com>
2023-02-23 17:12:04 -0800

    Create free_dlistint.c

---
+++ 0x00-challenge/4-delete_dnodeint/free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_dlistint - Free a list
+ *
+ * @head: A pointer to the first element of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}

d7be7bae156a391c10e3f47a0899f7b49bd4aaf1
Itodo Emmanuel Itodo <54606115+itodoemmanuel@users.noreply.github.com>
2023-02-23 17:13:18 -0800

    Create main.c

---
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -0,0 +1,111 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    if (!add_dnodeint_end(&head, 0) ||
+        !add_dnodeint_end(&head, 1) ||
+        !add_dnodeint_end(&head, 2) ||
+        !add_dnodeint_end(&head, 3) ||
+        !add_dnodeint_end(&head, 4) ||
+        !add_dnodeint_end(&head, 98) ||
+        !add_dnodeint_end(&head, 402) ||
+        !add_dnodeint_end(&head, 1024)) {
+        perror("add_dnodeint_end");
+        return EXIT_FAILURE;
+    }
+    print_dlistint(head);
+    printf("-----------------\n");
+
+    int idx;
+    for (idx = 5; idx >= 0; idx--) {
+        if (!delete_dnodeint_at_index(&head, idx)) {
+            perror("delete_dnodeint_at_index");
+            break;
+        }
+        print_dlistint(head);
+        printf("-----------------\n");
+    }
+
+    free_dlistint(head);
+    return EXIT_SUCCESS;
+}
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+	add_dnodeint_end(&head, 0);
+	add_dnodeint_end(&head, 1);
+	add_dnodeint_end(&head, 2);
+	add_dnodeint_end(&head, 3);
+	add_dnodeint_end(&head, 4);
+	add_dnodeint_end(&head, 98);
+	add_dnodeint_end(&head, 402);
+	add_dnodeint_end(&head, 1024);
+	print_dlistint(head);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 5);
+	print_dlistint(head);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	print_dlistint(head);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	print_dlistint(head);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	print_dlistint(head);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	print_dlistint(head);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	print_dlistint(head);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	print_dlistint(head);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	printf("-----------------\n");
+	delete_dnodeint_at_index(&head, 0);
+	print_dlistint(head);
+	free_dlistint(head);
+	return (0);
+}
